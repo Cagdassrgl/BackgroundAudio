48b2b40d89346a4a8502639090d1f2b88333ead5
Çağdaş Sarıgil <45997844+Cagdassrgl@users.noreply.github.com>
2023-06-13 00:05:19 +0300

    background audio

--- BackgroundAudio/ViewController.swift
+++ BackgroundAudio/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer : AVAudioPlayer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        do {
+            let filePath = Bundle.main.path(forResource: "music", ofType: ".mp3")
+            guard let path = filePath else { return }
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: path))
+            audioPlayer?.prepareToPlay()
+            
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 
-
+    @IBAction func play(_ sender: Any) {
+        audioPlayer?.play()
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        audioPlayer?.stop()
+    }
 }
 
